5cb34fbda6b12a9d129bb71edba5d39ea7172db5
syueer <syue531@hotmail.com>
2022-06-24 23:53:21 +1000

    challenge

--- challenge/index.js
+++ challenge/index.js
@@ -4,9 +4,31 @@
  */
 
 // Selectors
-
+const headline = document.querySelector('#headline')
+const button = document.querySelector('#button')
+const list = document.querySelector('#list')
+let items = list.getElementsByTagName('li')
+let length = items.length
 // Iterate the ul items
 
 // Event Listener - Click the item change the heading
-
+document.body.onclick = (e) => {
+  if (e.target.tagName === 'LI') {
+    headline.innerText = e.target.innerHTML
+  }
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].innerText === headline.innerText) {
+      items[i].style.backgroundColor = 'yellow'
+    } else {
+      items[i].style.backgroundColor = ''
+    }
+  }
+}
 // Event Listener - Click the button add a new item
+button.onclick = () => {
+  let node = document.createElement('li')
+  node.innerText = `${++length}th Item`
+  node.classList.add('border-bottom')
+  node.classList.add('m-3')
+  list.appendChild(node)
+}
