c1a0c75fad9a5d91c364852b227fab975f848aae
Chris <fablekey@gmail.com>
2022-06-24 15:12:58 +1000

    initial commit

---
+++ activity1/index.js
@@ -0,0 +1,84 @@
+// BASIC SYNTAX
+// Selectors: 
+let elementName = document.querySelector("#elementId")
+// Functions
+function functionName() {
+  // do something here
+ 
+}
+// Call the event with an event listener
+elementName.addEventListener("onclick", functionName);
+
+// 1. DISPLAY NAME
+// Click the button to display your name in the box
+// (Tip: in this example, you will hard code your name within the JavaScript function, rather than use the input box to grab it).
+
+// Selectors
+
+// Function
+
+// Call the event
+
+// 2. LIGHTSWITCH
+// Switch lights on mouseover
+// When the mouse cursor is on the lightbulb image, the lighton image should show; and revert back to the lightoff image when the mouse moves off.
+
+// Selectors
+
+// Function
+
+// Call the event
+
+// 3. Double click button to display content
+// Selectors
+
+/* Function {
+  //some code here
+  //create a new img element
+}*/
+
+// Call the event
+
+// 4. Traffic Lights
+// Selectors
+
+// Function 1
+// Function 2
+// Function 3
+
+// Call the events
+
+// 5. Change textbox border colours
+// Selectors
+
+// Function
+
+// Call the event
+
+// 6. Validate the input [length should be more than 5 characters]
+// Selectors
+
+/*Function {
+  //if statement
+}*/
+
+// Call the event
+
+// 7.create a list of hobbies
+// Selectors
+
+/*Function {
+  //for loop
+}*/
+
+// Call the event
+
+// 8. Display a profile card from an object
+//Object
+
+// Selectors
+
+/*Function {
+    //for loop
+      //if statement
+  }*/
